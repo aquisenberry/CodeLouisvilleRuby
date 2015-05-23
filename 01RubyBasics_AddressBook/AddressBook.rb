891afff82385e7c47e4fe56172100d7631685f3d
Anthony Quisenberry <anthony.quisenberry01@gmail.com>
2015-05-23 12:19:53 -0400

    add AddContact method

--- 01RubyBasics_AddressBook/AddressBook.rb
+++ 01RubyBasics_AddressBook/AddressBook.rb
@@ -51,17 +51,78 @@ class AddressBook
 			puts item.to_s("fullName")
 		}
 	end
+	def addAddress(contact)
+		print "Type: "
+		kind = gets.chomp
+		print "Street 1: "
+		street1 = gets.chomp
+		print "Street 2: "
+		street2 = gets.chomp
+		print "City: "
+		city = gets.chomp
+		print "State: "
+		state = gets.chomp
+		print "Zipcode: "
+		zip = gets.chomp
+		contact.addAddress(kind,street1,street2,city,state,zip)
+	end
+	def addPhone(contact)
+		print "Type: "
+		kind = gets.chomp
+		print "Phone Number: "
+		number = gets.chomp
+		contact.addPhoneNumber(kind,number)
+	end
+	def addContact
+		contact = Contact.new
+		print "First Name: "
+		contact.firstName = gets.chomp
+		print "Middle Name: "
+		contact.middleName = gets.chomp
+		print "Last Name: "
+		contact.lastName = gets.chomp
+		additionalDetails(contact,"address")
+		additionalDetails(contact, "phone number")
+		contacts.push(contact)
+	end
+	def aOrAn(str)
+		if str[0,1] == "a" || str[0,1] == "e" || str[0,1] == "i" || str[0,1] == "o" || str[0,1] == "u"
+			return "an"
+		end
+		return "a"
+	end
+	def additionalDetails(contact,str)
+		print "Would You like to add #{aOrAn(str)} #{str}? [Y/n]: "
+		response = gets.chomp
+		while response.downcase == "y" || response == ""
+			case str
+			when "address"
+				addAddress(contact)
+			when "phone number"
+				addPhone(contact)
+			end
+			print "Would you like to add another #{str}? [Y/n]: "
+			response = ""
+			response = gets.chomp
+		end
 
+	end
 	def run
 		loop do
 			puts "Address Book"
 			puts "e: Exit"
+			puts "c: Add Contact"
+			puts "p: Print Address Book"
 			print "Enter Your Choice: "
 			input = gets.chomp.downcase
 
 			case input
 			when "e"
 				break
+			when "p"
+				printContactList
+			when "c"
+				addContact
 			end
 		end
 	end
--- 01RubyBasics_AddressBook/_testing.rb
+++ 01RubyBasics_AddressBook/_testing.rb
@@ -1,46 +1,46 @@
 require "./Contact"
 require "./AddressBook"
-contacts = []
-anthony = {firstName: "Anthony", middleName: "Quinton", lastName: "Quisenberry", home: "(111)111-1111", work: "(222)222-2222", cell: "(333)333-3333"}
-samantha = {firstName: "Samantha", middleName: "Marie", lastName: "Semones", home: "", work: "(222)222-2222", cell: "(333)333-3333"}
-kylah = {firstName: "Kylah", middleName: "Sophia", lastName: "Brown", home: "(111)111-1111", work: "", cell: ""}
-awes = {firstName: "Awes", middleName: "", lastName: "Saidi", home: "(111)111-1111", work: "", cell: "(333)333-3333"}
+# contacts = []
+# anthony = {firstName: "Anthony", middleName: "Quinton", lastName: "Quisenberry", home: "(111)111-1111", work: "(222)222-2222", cell: "(333)333-3333"}
+# samantha = {firstName: "Samantha", middleName: "Marie", lastName: "Semones", home: "", work: "(222)222-2222", cell: "(333)333-3333"}
+# kylah = {firstName: "Kylah", middleName: "Sophia", lastName: "Brown", home: "(111)111-1111", work: "", cell: ""}
+# awes = {firstName: "Awes", middleName: "", lastName: "Saidi", home: "(111)111-1111", work: "", cell: "(333)333-3333"}
 
-contacts.push(anthony,samantha,kylah,awes)
+# contacts.push(anthony,samantha,kylah,awes)
 
 addressBook = AddressBook.new
-contact = Contact.new 
+# contact = Contact.new 
 
-# puts contact.fullName
-# puts contact.lastFirst
+# # puts contact.fullName
+# # puts contact.lastFirst
 
-contacts.each do |con|
-	contact1 = Contact.new(con[:firstName],con[:middleName],con[:lastName])
-	if con[:home].length > 0 then
-		contact1.addPhoneNumber("home",con[:home])
-	end
-	if con[:work].length > 0 then 
-		contact1.addPhoneNumber("work",con[:work])
-	end
-	if con[:cell].length > 0 then 
-		contact1.addPhoneNumber("cell",con[:cell])
-	end
-	contact1.addAddress("Home", "123 Main St.", "", "Portland", "OR", "12345")
-	addressBook.contacts.push(contact1)
-end
+# contacts.each do |con|
+# 	contact1 = Contact.new(con[:firstName],con[:middleName],con[:lastName])
+# 	if con[:home].length > 0 then
+# 		contact1.addPhoneNumber("home",con[:home])
+# 	end
+# 	if con[:work].length > 0 then 
+# 		contact1.addPhoneNumber("work",con[:work])
+# 	end
+# 	if con[:cell].length > 0 then 
+# 		contact1.addPhoneNumber("cell",con[:cell])
+# 	end
+# 	contact1.addAddress("Home", "123 Main St.", "", "Portland", "OR", "12345")
+# 	addressBook.contacts.push(contact1)
+# end
 
-jason = Contact.new
-jason.firstName = "Jason"
-jason.lastName = "Seifer"
-jason.addPhoneNumber("Home", "123-456-7890")
-jason.addPhoneNumber("Work", "456-789-0123")
-jason.addAddress("Home", "123 Main St.", "", "Portland", "OR", "12345")
+# jason = Contact.new
+# jason.firstName = "Jason"
+# jason.lastName = "Seifer"
+# jason.addPhoneNumber("Home", "123-456-7890")
+# jason.addPhoneNumber("Work", "456-789-0123")
+# jason.addAddress("Home", "123 Main St.", "", "Portland", "OR", "12345")
 
 # puts jason.to_s('full_name')
 # jason.printPhoneNumbers
 # jason.printAddresses
 
-addressBook.contacts.push(jason)
+# addressBook.contacts.push(jason)
 #test printContactList
 #addressBook.printContactList
 
