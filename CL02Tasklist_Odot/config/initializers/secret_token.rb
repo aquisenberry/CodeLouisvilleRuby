4a8e4d7b0f0fb7c2476062ac2ac6784f8cdda3fe
Anthony Quisenberry <anthony.quisenberry01@gmail.com>
2015-05-25 19:04:15 -0400

    initial commit of scaffolding

---
+++ CL02Tasklist_Odot/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CL02TasklistOdot::Application.config.secret_key_base = '019e45ffbb4209fd420490040c9242ee13bc8b636ade21075316da4c47d1012562ccae6e05e79f81e68c63d3bd33b9b0f25eece395b7627a86fbacb078da510f'
---
+++ CL02Tasklist_Odot/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CL02TasklistOdot::Application.config.session_store :cookie_store, key: '_CL02Tasklist_Odot_session'
