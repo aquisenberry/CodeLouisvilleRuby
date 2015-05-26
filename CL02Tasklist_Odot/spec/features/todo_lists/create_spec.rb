2823705ee22ddb5248e41aeb3b2b2ab8384aaaed
Anthony Quisenberry <anthony.quisenberry01@gmail.com>
2015-05-26 14:04:25 -0400

    Add Description presence validation

--- CL02Tasklist_Odot/spec/features/todo_lists/create_spec.rb
+++ CL02Tasklist_Odot/spec/features/todo_lists/create_spec.rb
@@ -48,4 +48,23 @@ describe "Creating todo lists" do
 		expect(page).to_not have_content("This is what I'm doing today.")
 
 	end
+
+	it "Displays an error when the todo list has no description"do
+		expect(TodoList.count).to eq(0)		
+
+		visit "/todo_lists"
+		click_link "New Todo list"
+		expect(page).to have_content("New todo_list")		
+
+		fill_in "Title", with: "Hello"
+		fill_in "Description", with: ""
+		click_button "Create Todo list"
+
+		expect(page).to have_content("error")		
+		expect(TodoList.count).to eq(0)	
+
+		visit "/todo_lists"
+		expect(page).to_not have_content("Hello")
+
+	end
 end
