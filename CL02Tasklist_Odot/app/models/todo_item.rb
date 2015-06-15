5e06888436e418a6da8415f50b78cd0e696fd44d
Anthony Quisenberry <anthony.quisenberry01@gmail.com>
2015-06-15 18:40:45 -0500

    Complete Module

--- CL02Tasklist_Odot/app/models/todo_item.rb
+++ CL02Tasklist_Odot/app/models/todo_item.rb
@@ -2,4 +2,8 @@ class TodoItem < ActiveRecord::Base
   belongs_to :todoList
   validates :content, presence: true,
   				length:{minimum: 2}
+
+  scope :complete, ->{ where("completed_at is not null")}
+  scope :incomplete, ->{where(completed_at: nil)}
+
 end
--- CL02Tasklist_Odot/spec/models/todo_list_spec.rb
+++ CL02Tasklist_Odot/spec/models/todo_list_spec.rb
@@ -2,4 +2,32 @@ require 'spec_helper'
 
 describe TodoList do
   it{should have_many(:todoItems).with_foreign_key('todoList_id')}
+
+  describe "#has_completed_items?" do
+  	let!(:todoList){ TodoList.create(title: "Groceries", description: "Grocery List.")}
+
+  	it "returns true with completed todo list items" do
+		todoList.todoItems.create(content: "Eggs", completed_at: 1.minute.ago)
+		expect(todoList.has_completed_items?).to be_truthy	
+  	end
+
+  	it "returns false with no completed todo list items" do
+		todoList.todoItems.create(content: "Eggs")
+		expect(todoList.has_completed_items?).to be_falsey		
+  	end
+  end
+
+  describe "#has_incompleted_items?" do
+  	let!(:todoList){ TodoList.create(title: "Groceries", description: "Grocery List.")}
+
+  	it "returns true with incompleted todo list items" do
+		todoList.todoItems.create(content: "Eggs")
+		expect(todoList.has_incompleted_items?).to be_truthy 		
+  	end
+
+  	it "returns false with no completed todo list items" do
+		todoList.todoItems.create(content: "Eggs", completed_at: 1.minute.ago)
+		expect(todoList.has_incompleted_items?).to be_falsey 		
+  	end
+  end
 end
