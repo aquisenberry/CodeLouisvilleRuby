aa840ef857c71513cca7765ddbe6fe961bf6fb90
Anthony Quisenberry <anthony.quisenberry01@gmail.com>
2015-07-14 22:18:58 -0400

    Add Login requirement - break tests

--- CL05Authentication_Odot/spec/features/todo_lists/edit_spec.rb
+++ CL05Authentication_Odot/spec/features/todo_lists/edit_spec.rb
@@ -1,23 +1,39 @@
 require 'spec_helper'
 
-describe "Editing todo lists" do
-  let!(:todo_list) { TodoList.create(title: "Groceries", description: "Grocery list.") }
 
+describe "Editing todo lists" do
+  before do
+    
+  end
+  let!(:todo_list) { TodoList.create(title: "Groceries", description: "Grocery list.") }
+  let!(:user){User.create(first_name: "Anthony", last_name: "Quisenberry", email: "anthony.quisenberry@gmail.com", password: "treehouse1", password_confirmation: "treehouse1")}
+  
   def update_todo_list(options={})
     options[:title] ||= "My todo list"
     options[:description] ||= "This is my todo list."
     todo_list = options[:todo_list]
 
+
+    User.stub(:find_by).and_return(user)
     visit "/todo_lists"
+    expect(page).to have_content("asdf")
     within "#todo_list_#{todo_list.id}" do
       click_link "Edit"
     end
+    
 
     fill_in "Title", with: options[:title]
     fill_in "Description", with: options[:description]
     click_button "Update Todo list"
   end
 
+  # def log_in 
+  #   let!(:user){User.create(first_name: "Anthony", last_name: "Quisenberry", email: "anthony.quisenberry@gmail.com", password: "treehouse1", password_confirmation: "treehouse1")}
+  #   visit new_user_session_path
+  #   fill_in "Email Address", with: user.email
+  #   fill_in "Password", with: user.password
+  #   expect(page).to have_content("asdfas")
+  # end
   it "updates a todo list successfully with correct information" do
     update_todo_list todo_list: todo_list, 
                      title: "New title", 
--- CL05Authentication_Odot/spec/features/users/registration_spec.rb
+++ CL05Authentication_Odot/spec/features/users/registration_spec.rb
@@ -4,16 +4,15 @@ describe "Signing up" do
 	it "allows user to sign up for the site and creates the object in the database" do
 		expect(User.count).to eq(0)
 
-		visit "/"
-		expect(page).to have_content("Sign Up")	
-		click_link "Sign Up"
+		visit new_user_path
+		expect(page).to have_content("First Name")	
+		# click_link "Sign Up"
 		fill_in "First Name", with: "Jason"
 		fill_in	"Last Name", with: "Seifer"
 		fill_in "Email", with: "jason@teamtreehouse.com"
 		fill_in "Password", with: "treehouse1234"
-		fill_in "Password (again)", with: "teehouse1234"
+		fill_in "Password (again)", with: "treehouse1234"
 		click_button "Sign Up"
-
 		expect(User.count).to eq(1) 
 	end
 end
